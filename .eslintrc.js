2515ab75dcc9af41b7455707cb6de3ca2a9de811
mpalmr <matthew.palmer1@mohawkcollege.ca>
2024-05-15 10:10:11 -0400

    add eslint-config-airbnb with typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,43 @@
 'use strict';
 
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'plugin:import/electron',
-    'plugin:import/typescript',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      env: {
+        browser: true,
+        es6: true,
+        node: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'airbnb',
+        'airbnb-typescript',
+        'plugin:import/electron',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    {
+      files: ['*.js'],
+      env: {
+        es6: true,
+        node: true,
+      },
+      extends: [
+        'airbnb',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      rules: {
+        strict: [2, 'global'],
+      },
+    },
   ],
-  parser: '@typescript-eslint/parser',
-}
+};
