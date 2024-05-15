fd8a33d6de6d4e1f22274b913c67c1c399478339
mpalmr <matthew.palmer1@mohawkcollege.ca>
2024-05-15 10:17:46 -0400

    render minimal react app

--- src/renderer.ts
+++ src/renderer.ts
@@ -27,5 +27,6 @@
  */
 
 import './index.css';
+import render from './ui';
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+document.addEventListener('DOMContentLoaded', render);
---
+++ src/ui/app.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const App: FC = function App() {
+  return (
+    <h1>It works!</h1>
+  );
+};
+
+export default App;
